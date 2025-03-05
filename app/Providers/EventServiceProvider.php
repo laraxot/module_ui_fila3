648db9d162ed56f2470a68d11440a14d4fdde70b
marco76tv <marco.sottana@gmail.com>
2025-03-05 19:21:59 +0100

    .

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Modules\UI\Providers;
 
+use Modules\Xot\Providers\XotBaseEventServiceProvider;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as BaseEventServiceProvider;
 
-class EventServiceProvider extends BaseEventServiceProvider
+class EventServiceProvider extends XotBaseEventServiceProvider
 {
+    public string $name = 'UI';
+
     /**
      * The event handler mappings for the application.
      *
