54b3124c75df88fa42d85f5d08cc16e8cd65518e
marco76tv <1944941+marco76tv@users.noreply.github.com>
2025-03-05 18:22:27 +0000

    Lint

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Modules\UI\Providers;
 
 use Modules\Xot\Providers\XotBaseEventServiceProvider;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider as BaseEventServiceProvider;
 
 class EventServiceProvider extends XotBaseEventServiceProvider
 {
